3fadefd83dd172b2f86d571bbc1717c149f2e2d1
Eduardo Fonseca <efonsecab@gmail.com>
2023-06-25 20:40:28 -0600

    Integrating Blazor Authorization components and router

---
+++ Client/CustomProviders/AzureSWAAuthenticationStateProvider.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Json;
+using System.Security.Claims;
+
+namespace BlazorApp.Client.CustomProviders
+{
+    /*
+     * Check
+     * https://github.com/anthonychu/blazor-auth-static-web-apps/blob/main/src/StaticWebAppsAuthenticationExtensions/StaticWebAppsAuthenticationStateProvider.cs
+     * https://anthonychu.ca/post/blazor-auth-azure-static-web-apps/
+     * */
+    public class AzureSWAAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly string _baseAddress;
+        private readonly HttpClient _httpClient;
+        public AzureSWAAuthenticationStateProvider(string baseAddress)
+        {
+            this._baseAddress = baseAddress;
+            this._httpClient = new HttpClient() { BaseAddress = new Uri(this._baseAddress) };
+        }
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            try
+            {
+                string requestUrl = "/.auth/me";
+                var authData = await this._httpClient.GetFromJsonAsync<AuthData>(requestUrl);
+                var identity = new ClaimsIdentity(authData!.ClientPrincipal!.IdentityProvider);
+                identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, authData.ClientPrincipal.UserId!));
+                identity.AddClaim(new Claim(ClaimTypes.Name, authData.ClientPrincipal.UserDetails!));
+                identity.AddClaims(authData.ClientPrincipal.UserRoles!
+                    .Select(role => new Claim(ClaimTypes.Role, role)));
+                return new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+            catch (Exception)
+            {
+                return new AuthenticationState(new ClaimsPrincipal());
+            }
+        }
+
+
+        public class AuthData
+        {
+            public Clientprincipal? ClientPrincipal { get; set; }
+        }
+
+        public class Clientprincipal
+        {
+            public string? UserId { get; set; }
+            public string[]? UserRoles { get; set; }
+            public object[]? Claims { get; set; }
+            public string? IdentityProvider { get; set; }
+            public string? UserDetails { get; set; }
+        }
+
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using BlazorApp.Client.CustomProviders;
+using System.ComponentModel.DataAnnotations;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddAuthorizationCore()
+    .AddScoped<AuthenticationStateProvider, AzureSWAAuthenticationStateProvider>(sp => 
+    {
+        var env = sp.GetService<IWebAssemblyHostEnvironment>();
+        return new AzureSWAAuthenticationStateProvider(env!.BaseAddress);
+    });
 await builder.Build().RunAsync();
