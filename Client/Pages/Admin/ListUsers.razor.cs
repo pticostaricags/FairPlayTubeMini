768c110dff1eaf701c27f72e5c35f26c12124e9c
Eduardo Fonseca <efonsecab@gmail.com>
2023-06-27 17:30:43 -0600

    Adding ability to list users

---
+++ Client/Pages/Admin/ListUsers.razor.cs
@@ -0,0 +1,40 @@
+using BlazorApp.Shared.DataModels;
+using Blazored.Toast.Services;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace BlazorApp.Client.Pages.Admin
+{
+    public partial class ListUsers
+    {
+        [Inject]
+        private IToastService? ToastService { get; set; }
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+        private bool IsBusy { get; set; }
+        private ApplicationUserList? Users { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                this.IsBusy = true;
+                await Task.Delay(TimeSpan.FromHours(20));
+                string baseAddress=$"{NavigationManager!.BaseUri}";
+                HttpClient httpClient = new HttpClient()
+                {
+                    BaseAddress = new Uri(baseAddress)
+                };
+                this.Users = await httpClient.GetFromJsonAsync<ApplicationUserList>("data-api/rest/ApplicationUser");
+            }
+            catch (Exception ex)
+            {
+                this.ToastService!.ShowError(ex.Message);
+            }
+            finally
+            {
+                this.IsBusy = false;
+            }
+        }
+    }
+}
---
+++ Shared/DataModels/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared.DataModels
+{
+
+    public class ApplicationUserList
+    {
+        public ApplicationUser[] value { get; set; }
+    }
+
+    public class ApplicationUser
+    {
+        public int ApplicationUserId { get; set; }
+        public string Username { get; set; }
+    }
+
+
+}
