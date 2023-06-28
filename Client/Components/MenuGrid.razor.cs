5687280c9d472aee9346cb86a51f6b9a3cb8d1b6
Eduardo Fonseca <efonsecab@gmail.com>
2023-06-28 17:28:24 -0600

    Modifying draft ui. Integrating menu grid component

---
+++ Client/Components/MenuGrid.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Client.Components
+{
+    public partial class MenuGrid
+    {
+        [EditorRequired]
+        [Parameter]
+        public MenuGridItem[]? MenuItems { get; set; }
+
+        public class MenuGridItem
+        {
+            public string? Title { get; set; } = string.Empty;
+            public string? CssClass { get; set; } = string.Empty;
+            public EventCallback OnClick { get; set; }
+            public bool ShowTitleBelowIcon { get; set; } = false;
+            public string? ImageSrc { get; set; }
+            public string? ImageCss { get; set; }
+            public bool ShowImage { get; set; }
+        }
+    }
+}
--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -1,9 +1,67 @@
-﻿using Blazored.Toast.Services;
+﻿using BlazorApp.Client.Components;
+using Blazored.Toast.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace BlazorApp.Client.Pages
 {
     public partial class Index
     {
+        [CascadingParameter]
+        private Task<AuthenticationState>? AuthenticationStateTask { get; set; }
+        [Inject]
+        private IToastService? ToastService { get; set; }
+        [Inject]
+        private NavigationManager? NavigationManager { get; set; }
+        private bool IsBusy { get; set; }
+        private MenuGrid.MenuGridItem[] MenuGridItems = new MenuGrid.MenuGridItem[0];
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                this.IsBusy = true;
+                var state = await this.AuthenticationStateTask!;
+                if (state?.User?.Identity?.IsAuthenticated == true)
+                {
+                    if (state.User.IsInRole("creator"))
+                    {
+                        this.MenuGridItems = 
+                        this.MenuGridItems.Union(new MenuGrid.MenuGridItem[] 
+                        {
+                            new MenuGrid.MenuGridItem()
+                            {
+                                CssClass="bi bi-cloud-upload-fill",
+                                OnClick=new EventCallback(this,()=>this.NavigationManager!.NavigateTo("/User/UploadVideo")),
+                                ShowTitleBelowIcon=true,
+                                Title="Upload Video"
+                            }
+                        })
+                        .ToArray();
+                    }
+                }
+                else
+                {
+                    this.MenuGridItems = new MenuGrid.MenuGridItem[]
+                    {
+                        new MenuGrid.MenuGridItem()
+                        {
+                            Title="Videos List",
+                            CssClass="bi bi-play-btn-fill",
+                            ShowTitleBelowIcon=true,
+                            OnClick=new EventCallback(this,()=>this.NavigationManager!.NavigateTo("/VideosList")),
+                        }
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                this.ToastService!.ShowError(ex.Message);
+            }
+            finally
+            {
+                this.IsBusy = false;
+            }
+        }
     }
 }
