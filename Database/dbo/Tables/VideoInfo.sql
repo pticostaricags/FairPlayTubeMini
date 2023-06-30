ead4f0e8cd4ca0e681eef9d97dd9007fa5c5e222
Eduardo Fonseca <efonsecab@gmail.com>
2023-06-30 07:32:07 -0600

    Adding VideoInfo table

---
+++ Database/dbo/Tables/VideoInfo.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[VideoInfo]
+(
+	[VideoInfoId] BIGINT NOT NULL CONSTRAINT PK_VideoInfo PRIMARY KEY, 
+    [OwnerApplicationUserId] BIGINT NOT NULL, 
+    [AccountId] UNIQUEIDENTIFIER NOT NULL, 
+    [VideoId] NVARCHAR(50) NOT NULL, 
+    [Location] NVARCHAR(50) NOT NULL, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(500) NOT NULL, 
+    CONSTRAINT [FK_VideoInfo_ApplicationUser] FOREIGN KEY ([OwnerApplicationUserId]) REFERENCES [ApplicationUser]([ApplicationUserId])
+)
